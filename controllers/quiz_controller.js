a6fb7986d7a8a9994ad7ab455458bbd7ac087b32
yinovic <a_bermejo46@hotmail.com>
2015-06-13 17:31:56 +0200

    Añadir autor

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,17 @@
 /* GET /quizes/question */
 exports.question = function(req, res) {
   res.render('quizes/question', { pregunta: 'Capital de Italia' });
-  };
+};
 
   /* GET /quizes/answer */
-  exports.answer = function(req, res) {
-    if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', { respuesta: 'Correcta' });
-    }else{
-      res.render('quizes/answer', { respuesta: 'Incorrecto' });
-    }
-    };
+exports.answer = function(req, res) {
+  if (req.query.respuesta === 'Roma'){
+    res.render('quizes/answer', { respuesta: 'Correcta' });
+  }else{
+    res.render('quizes/answer', { respuesta: 'Incorrecto' });
+  }
+};
+
+exports.author = function(req, res) {
+  res.render('author', { autor: 'Álvaro Bermejo' });
+};
