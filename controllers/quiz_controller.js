4855c3e949cfd70a5f4b2a560429b4ae73120e8a
yinovic <a_bermejo46@hotmail.com>
2015-06-20 17:15:33 +0200

    Añadido filtro

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,46 @@
 var models = require('../models/models.js');
 
+// Autoload - factorización del codigo
+exports.load = function(req, res, next, quizId){
+  models.Quiz.findById(quizId).then(
+    function (quiz){
+      if (quiz){
+        req.quiz = quiz;
+        next();
+      }else{
+        next(new Error('No existe quizId='+quizId));
+      }
+    }
+  ).catch(function (error) { next(error);});
+};
+
 /* GET /quizes/question */
-exports.question = function(req, res  ) {
-  models.Quiz.findAll().then(function (quiz) {
-    res.render('quizes/question', { pregunta: quiz[0].pregunta})
-  })
+exports.index = function(req, res  ) {
+  var search = req.query.search;
+  if ((typeof(search) === "undefined") ){
+    search = "%";
+  }else{
+    search = search.replace(/\s/g,"%");
+    search="%"+search+"%";
+  }
+
+  models.Quiz.findAll({where: ["pregunta like ?", search],order: '`pregunta` ASC'}).then(function (quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
+  }).catch(function (error) { next(error);});
+};
+
+/* GET /quizes/question */
+exports.show = function(req, res  ) {
+    res.render('quizes/show', { quiz: req.quiz});
 };
 
   /* GET /quizes/answer */
 exports.answer = function(req, res) {
-  models.Quiz.findAll().then(function (quiz) {
-    if (req.query.respuesta === quiz[0].respuesta){
-      res.render('quizes/answer', { respuesta: 'Correcta' });
-    }else{
-      res.render('quizes/answer', { respuesta: 'Incorrecto' });
+  var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta){
+      resultado = 'Correcto';
     }
-  })
+    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
 
 /* GET /author */
